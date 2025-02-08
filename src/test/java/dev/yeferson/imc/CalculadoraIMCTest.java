e18155f69edcc0068f7f863d9f079a0a2c9edb19
unknown <gaviriayeferson2@gmail.com>
2025-02-08 18:41:42 +0100

    add imcDelgadezSevera o verify correct classification according to imc , test fails

--- src/test/java/dev/yeferson/imc/CalculadoraIMCTest.java
+++ src/test/java/dev/yeferson/imc/CalculadoraIMCTest.java
@@ -2,6 +2,7 @@ package dev.yeferson.imc;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.DisplayName;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.instanceOf;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.closeTo;
@@ -15,19 +16,33 @@ public class CalculadoraIMCTest {
 
         //Given
         double peso = 70;
-        double altura = 1.75;
+        double estatura = 1.75;
         CalculadoraIMC calculadora =new CalculadoraIMC();
 
         //When
-        double imcCalculado = calculadora.calcularIMC(peso, altura);
+        double imcCalculado = calculadora.calcularIMC(peso, estatura);
 
         //Then
         double imcEsperado = 22.86;
         assertThat(calculadora, instanceOf(CalculadoraIMC.class));
         assertThat(imcCalculado, closeTo(imcEsperado, 0.01));
+    }
 
+    @Test
+    @DisplayName("Clasifica como 'Delgadez Severa' cuando el IMC es menor a 16")
 
-      
-        
+    void imcDelgadezSevera() {
+
+        //Given     
+        double peso = 40;
+        double estatura = 1.75;
+        CalculadoraIMC calculadora = new CalculadoraIMC();
+
+        //When
+        double imcCalculado = calculadora.calcularIMC(peso, estatura);
+        String clasificacion = calculadora.clasificarIMC(imc);
+
+        //Then
+        assertThat(clasificacion, is("Delgadez Severa"));
     }
 }
