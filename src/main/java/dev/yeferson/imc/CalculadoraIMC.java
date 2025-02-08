02a2c08e3d39a2fa9f05a0fcd393a0b14fb9889e
unknown <gaviriayeferson2@gmail.com>
2025-02-08 18:48:08 +0100

    add clasificarIMC  method to approve test

--- src/main/java/dev/yeferson/imc/CalculadoraIMC.java
+++ src/main/java/dev/yeferson/imc/CalculadoraIMC.java
@@ -6,4 +6,25 @@ public class CalculadoraIMC {
         return  peso / Math.pow(estatura, 2);
     }
 
+    public String clasificarIMC(double imc) {
+        if (imc < 16) {
+            return "Delgadez severa";
+        } else if (imc >= 16 && imc < 17) {
+            return "Delgadez moderada";
+        } else if (imc >= 17 && imc < 18.5) {
+            return "Delgadez leve";
+        } else if (imc >= 18.5 && imc < 25) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            return "Sobrepeso";
+        } else if (imc >= 30 && imc < 35) {
+            return "Obesidad leve";
+        } else if (imc >= 35 && imc < 40) {
+            return "Obesidad moderada";
+        } else {
+            return "Obesidad mórbida";
+        }
+    }
+    
+
 }
